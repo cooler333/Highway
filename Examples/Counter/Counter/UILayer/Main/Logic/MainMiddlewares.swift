111834ce1f766c4d234acc6279bcf35fecd4a2a8
Dmitrii Cooler <utm4@mail.ru>
2022-07-08 23:52:33 +0300

    Clean up example

---
+++ Examples/Counter/Counter/UILayer/Main/Logic/MainMiddlewares.swift
@@ -0,0 +1,57 @@
+//
+//  MainMiddlewares.swift
+//  Counter
+//
+//  Created by Dmitrii Cooler on 08.07.2022.
+//
+
+import Foundation
+import Highway
+
+extension MainFeature {
+    static func middlewares(
+        stateStorage: StateStorageProtocol
+    ) -> [Middleware<AppState, Action>] {
+        return [
+            onChangeSaveMiddleware(),
+            saveMiddleware(stateStorage: stateStorage)
+        ]
+    }
+    
+    private static func onChangeSaveMiddleware() -> Middleware<AppState, Action> {
+        createMiddleware({ dispatch, state, action in
+            switch action {
+            case .increment, .decrement:
+                dispatch(.save)
+            default:
+                break
+            }
+        })
+    }
+    
+    private static func saveMiddleware(
+        stateStorage: StateStorageProtocol
+    ) -> Middleware<AppState, Action> {
+        createMiddleware(
+            environment: stateStorage,
+            { dispatch, state, action, environment in
+                guard action == .save else { return }
+                DispatchQueue.global(
+                    qos: .background
+                ).asyncAfter(
+                    deadline: .now() + 2,
+                    execute: {
+                        let isSuccess = Int.random(in: 0...2) != 0
+                        if isSuccess {
+                            stateStorage.save(state)
+                            dispatch(.saved(success: true))
+                        } else {
+                            dispatch(.saved(success: false))
+                        }
+                    }
+                )
+            }
+        )
+
+    }
+}
