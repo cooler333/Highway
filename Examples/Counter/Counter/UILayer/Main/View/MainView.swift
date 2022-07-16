6d39ae16e8ef2c35be32e704014611b09a2b2026
Dmitrii Coolerov <utm4@mail.ru>
2022-07-16 18:27:12 +0300

    Add SwiftUI example

--- Examples/Counter/Counter/UILayer/Main/View/MainView.swift
+++ Examples/Counter/Counter/UILayer/Main/View/MainView.swift
@@ -10,67 +10,55 @@ import Foundation
 import SwiftUI
 import Highway
 
-final class MainViewController: UIViewController {
-
+struct MainView: View {
     private let store: Store<AppState, MainFeature.Action>
 
-    private var countLabel: UILabel!
+    @State var countString: String = ""
 
-    private let uiPublisher = PassthroughSubject<AppState, Never>()
-    private var cancellable = Set<AnyCancellable>()
-
-    init(store: Store<AppState, MainFeature.Action>) {
+    init(
+        store: Store<AppState, MainFeature.Action>
+    ) {
         self.store = store
-
-        super.init(nibName: nil, bundle: nil)
     }
 
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .secondarySystemBackground
-
-        setupCountLabel()
-
-        render(state: store.state)
-
-        store.subscribe(listener: { [weak self] state in
-            guard let self = self else { return }
-            self.uiPublisher.send(state)
-        })
-
-        uiPublisher
-            .receive(on: DispatchQueue.main)
-            .sink { state in
-                self.render(state: state)
+    var body: some View {
+        VStack {
+            Text(countString)
+        }
+        .background(Color.gray)
+        .onAppear {
+            configure()
+            store.subscribe { state in
+                DispatchQueue.main.async {
+                    configure()
+                }
             }
-            .store(in: &cancellable)
+        }
     }
 
-    private func setupCountLabel() {
-        let countLabel = UILabel()
-        view.addSubview(countLabel)
-        countLabel.translatesAutoresizingMaskIntoConstraints = false
-        let countLabelContstraints = [
-            countLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            countLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        ]
-        countLabelContstraints.forEach { $0.isActive = true }
-        countLabel.numberOfLines = 0
-        countLabel.textAlignment = .center
-
-        self.countLabel = countLabel
-    }
-
-    private func render(state: AppState) {
-        var text = "\(state.count)"
-        if state.isSaving == true {
+    private func configure() {
+        var text = "\(store.state.count)"
+        if store.state.isSaving == true {
             text += "\nSaving"
         }
-        countLabel.text = text
+        countString = text
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        let view = MainView(
+            store: .init(
+                reducer: .init({ state, action in
+                    return state
+                }),
+                state: .init(
+                    count: 12313112312,
+                    isSaving: true,
+                    saved: true
+                )
+            )
+        )
+        return view
     }
 }
